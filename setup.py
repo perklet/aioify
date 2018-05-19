205e1b5b0e7d2e0d84cc098251e4a21432d3873e
Yifei Kong <kongyifei@gmail.com>
2018-05-19 18:46:22 +0800

    bump version to 0.1.3

--- setup.py
+++ setup.py
@@ -11,22 +11,23 @@ with open(os.path.join(here, 'README.md')) as f:
 setup(
     name = 'aioify',
     packages = ['aioify'],
-    version = '0.1.2',
+    version = '0.1.3',
     description = 'Make every python function async/await',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
     author = 'Yifei Kong',
     author_email = 'kongyifei@gmail.com',
     url = 'https://github.com/yifeikong/aioify',
-    download_url = 'https://github.com/yifeikong/aioify/archive/0.1.tar.gz',
+    download_url = 'https://github.com/yifeikong/aioify/archive/0.1.3.tar.gz',
     keywords = ['async', 'await', 'wrap'],
     classifiers = [
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     ],
-    python_requires='>=3.6'
+    python_requires='>=3.5'
 )
