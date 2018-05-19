a5010aa9d43f4ff922a9d34332cd89f69ba0dd8b
Yifei Kong <kongyifei@gmail.com>
2018-05-19 18:25:24 +0800

    fill some missing values in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,32 @@
+import os
 from distutils.core import setup
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+
 setup(
-  name = 'aioify',
-  packages = ['aioify'],
-  version = '0.1',
-  description = 'Make every python function async/await',
-  author = 'Yifei Kong',
-  author_email = 'kongyifei@gmail.com',
-  url = 'https://github.com/yifeikong/aioify',
-  download_url = 'https://github.com/yifeikong/aioify/archive/0.1.tar.gz',
-  keywords = ['async', 'await', 'wrap'],
-  classifiers = [],
+    name = 'aioify',
+    packages = ['aioify'],
+    version = '0.1.2',
+    description = 'Make every python function async/await',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    author = 'Yifei Kong',
+    author_email = 'kongyifei@gmail.com',
+    url = 'https://github.com/yifeikong/aioify',
+    download_url = 'https://github.com/yifeikong/aioify/archive/0.1.tar.gz',
+    keywords = ['async', 'await', 'wrap'],
+    classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    ],
+    python_requires='>=3.6'
 )
