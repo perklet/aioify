8b3741275f8a7903e2fd18848b426297b2f318a0
Roman Inflianskas <infroma@gmail.com>
2018-10-29 11:50:45 +0300

    Add wrap_return_values argument to wrap function

--- aioify/__init__.pyi
+++ aioify/__init__.pyi
@@ -1,4 +1,7 @@
 from typing import Any, Callable
 
 
-def aioify(obj: Any, name: str = None, create_name_function: Callable[[Any], str] = None): ...
+def aioify(obj: Any,
+           name: str = None,
+           create_name_function: Callable[[Any], str] = None,
+           wrap_return_values: bool = False): ...
